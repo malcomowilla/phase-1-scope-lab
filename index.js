b573ad000a63f0ecf6fddf50a66225a17924b71b
malcomowilla <owillamalcom@gmail.com>
2022-11-29 08:36:19 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Write your solution in this file!phase-1-scope-lab
+
+var customerName = "bob"
+
+function upperCaseCustomerName (){
+    customerName = customerName.toLocaleUpperCase();
+}
+
+function setBestCustomer (){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer (){
+    bestCustomer = "maybe bob"
+}
+
+
+const leastFavoriteCustomer = "Job";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "jddjd";
+}
